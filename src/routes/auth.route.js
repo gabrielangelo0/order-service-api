5ecbe87fea2a14e7e1defd5956ec68d33b50955d
Gabriel Angelo <gabrielangelolira@gmail.com>
2025-08-16 10:40:46 -0300

    updated

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,29 +1,58 @@
-const express = require('express');
-const users = require('../data/users');
-const jwt = require('jsonwebtoken');
-const middlewareValidate = require('../middlewares/validate');
+const express = require("express");
+const users = require("../data/users");
+const jwt = require("jsonwebtoken");
+const middlewareValidate = require("../middlewares/validate");
+const { PrismaClient } = require("../generated/prisma");
+
+const prisma = new PrismaClient();
 
 const router = express.Router();
 
-router.post('/login', (req, res) => {
-    const { email, password } = req.body;
-    const user = users.find((usuario) => usuario.email === email && usuario.password === password);
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  const user = users.find(
+    (usuario) => usuario.email === email && usuario.password === password
+  );
 
-    if (!user) {
-        return res.status(401).send('Credenciais inválidas');
-    }
+  if (!user) {
+    return res.status(401).send("Credenciais inválidas");
+  }
 
-    const token = jwt.sign({
-        id: user.id,
-        email: user.email
-    }, 'secret_key', { expiresIn: '1h' });
+  const token = jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+    },
+    "secret_key",
+    { expiresIn: "1h" }
+  );
 
-    res.status(200).json({ token, user });
+  res.status(200).json({ token, user });
 });
 
-router.get('/auth/me', middlewareValidate, (req, res) => {
-    // Handle fetching user profile logic here
-    res.send('User profile endpoint');
+router.get("/auth/me", middlewareValidate, (req, res) => {
+  // Handle fetching user profile logic here
+  res.send("User profile endpoint");
+});
+
+router.post("/auth/register", async (req, res) => {
+  const { email, full_name, password } = req.body;
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email,
+        full_name,
+        password,
+        role: "admin"
+      },
+    });
+
+    res.status(201).json({ user });
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: error });
+  }
 });
 
 module.exports = router;
